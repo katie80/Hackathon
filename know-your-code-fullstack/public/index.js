d5301bae0144355acb68de92bad1c029baacf61c
Lawson Nerenberg <dartellis@gmail.com>
2016-10-21 12:31:19 -0600

    merge

--- know-your-code-fullstack/public/index.js
+++ know-your-code-fullstack/public/index.js
@@ -51,12 +51,10 @@ function randomizeOptions(){
 var score = 0;
 
 function updateScore() {
-  // if (correct) {
     score += (100 - timer.toFixed(1));
     return;
-  // }
-  // return;
 }
+
 $('#score').text(score);
 
 var timer = 60;
