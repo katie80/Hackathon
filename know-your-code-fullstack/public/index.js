267b8ff572f0efda4d72c18c1091e2a2734d48f7
Scott Loveless <scott@loveless.org>
2016-10-24 07:38:04 -0600

    updated answer div styles

--- know-your-code-fullstack/public/index.js
+++ know-your-code-fullstack/public/index.js
@@ -1,82 +1,82 @@
 /* jshint esversion:6 */
 $(function() {
-  var currentLevel = 0;
+	var currentLevel = 0;
 
-    var optionsArragnment = randomizeOptions();
-    var optionIds = ["answer1", "answer2", "answer3"];
+		var optionsArragnment = randomizeOptions();
+		var optionIds = ["answer1", "answer2", "answer3"];
 
-    $('#option1').attr("id", optionIds[optionsArragnment[0]]);
-    $('#option2').attr("id", optionIds[optionsArragnment[1]]);
-    $('#option3').attr("id", optionIds[optionsArragnment[2]]);
+		$('#option1').attr("id", optionIds[optionsArragnment[0]]);
+		$('#option2').attr("id", optionIds[optionsArragnment[1]]);
+		$('#option3').attr("id", optionIds[optionsArragnment[2]]);
 
-    $.get('/levels', function(res) {
-      $('#level_div').text(res[currentLevel].title);
-      $('#question').text(res[currentLevel].question);
-      $('#answer1').html(res[currentLevel].options[0]);
-      $('#answer2').html(res[currentLevel].options[1]);
-      $('#answer3').html(res[currentLevel].options[2]);
-    });
+		$.get('/levels', function(res) {
+			$('#level_div').text(res[currentLevel].title);
+			$('#question').text(res[currentLevel].question);
+			$('#answer1').html(res[currentLevel].options[0]);
+			$('#answer2').html(res[currentLevel].options[1]);
+			$('#answer3').html(res[currentLevel].options[2]);
+		});
 
-    $('.answers').click(function(){
-      if($(this).attr('id') === "answer1"){
-        $(this).css("background-color","green");
-        updateScore();
-      } else {
-        $(this).css("background-color","red");
-      }
-    });
+		$('.answers').click(function(){
+			if($(this).attr('id') === "answer1"){
+				$(this).css("box-shadow","0 0 5px 5px #379B4A" 
+					);
+				updateScore();
+			}
+			// Code to move on to next level goes here
+		});
 
-    $('#next_question_btn').click(function(){
-      currentLevel++;
-      $.get('/levels', function(res) {
-          $('#level_div').text(res[currentLevel].title);
-          $('#question').text(res[currentLevel].question);
-          $('#answer1').html(res[currentLevel].options[0]);
-          $('#answer2').html(res[currentLevel].options[1]);
-          $('#answer3').html(res[currentLevel].options[2]);
-        });
-    });
+		$('#next_question_btn').click(function(){
+			currentLevel++;
+			$.get('/levels', function(res) {
+					$('#level_div').text(res[currentLevel].title);
+					$('#question').text(res[currentLevel].question);
+					$('#answer1').html(res[currentLevel].options[0]);
+					$('#answer2').html(res[currentLevel].options[1]);
+					$('#answer3').html(res[currentLevel].options[2]);
+				});
+		});
 });
-
+	
 function randomizeOptions(){
-  var randPermutation = [];
+	var randPermutation = [];
 
-  while(randPermutation.length < 3){
+	while(randPermutation.length < 3){
 
-    var randIndex = Math.floor(Math.random() * 3);
-  
-    if(!randPermutation.includes(randIndex)){
-      randPermutation.push(randIndex);
-    }
-  }
+		var randIndex = Math.floor(Math.random() * 3);
+	
+		if(!randPermutation.includes(randIndex)){
+			randPermutation.push(randIndex);
+		}
+	}
 
-  console.log(randPermutation);
-  return randPermutation;
+	console.log(randPermutation);
+	return randPermutation;
 }
 
 var score = 0;
 
 function updateScore() {
-    score += (100 - timer);
-    clearInterval(timerInterval);
-    console.log(score);
-    $('#score').text(score);
-    return;
+		score += (100 - timer);
+		clearInterval(timerInterval);
+		console.log(score);
+		$('#score').text(score);
+		return;
 }
 
 $('#score').text(score);
 
 var timer = 0;
 var timerInterval = setInterval(() => {
-  if(timer > 99) {
-   clearInterval(timerInterval);
-  } else {
-    minuteTimer();
-  }
+	if(timer > 99) {
+	 clearInterval(timerInterval);
+	} else {
+		minuteTimer();
+	}
 }, 1000);
 
 function minuteTimer() {
-  timer += 1;
-  $('#timer').text(timer);
-  $('#fuse').css("width", 100 - timer/0.6 + "%");
+	timer += 1;
+	$('#timer').text(timer);
+	// $('#fuse').css("width", 100 - timer/0.6 + "%");
 }
