f99229540ea1e0270a3448d312cf6b00d4e98924
Lawson Nerenberg <dartellis@gmail.com>
2016-10-24 15:51:26 -0600

    fixed option randomizer

--- know-your-code-fullstack/public/index.js
+++ know-your-code-fullstack/public/index.js
@@ -4,12 +4,7 @@ $(function() {
 	var currentLevel = 0;
 	var clicked = false;
 
-	var optionsArragnment = randomizeOptions();
-	var optionIds = ["answer1", "answer2", "answer3"];
-
-	$('#option1').attr("id", optionIds[optionsArragnment[0]]);
-	$('#option2').attr("id", optionIds[optionsArragnment[1]]);
-	$('#option3').attr("id", optionIds[optionsArragnment[2]]);
+	randomizeOptions();
 
 	$.get('/levels', function(res) {
 		$('#level_div').html(res[currentLevel].title);
@@ -42,10 +37,15 @@ $(function() {
 			$('#answer2').html(res[currentLevel].options[1]);
 			$('#answer3').html(res[currentLevel].options[2]);
 		});
+		randomizeOptions();
 	});
 });
 	
 function randomizeOptions(){
+	$('#answer1').attr("id", "option1");
+	$('#answer2').attr("id", "option2");
+	$('#answer3').attr("id", "option3");
+
 	var randPermutation = [];
 	while(randPermutation.length < 3){
 		var randIndex = Math.floor(Math.random() * 3);
@@ -54,8 +54,12 @@ function randomizeOptions(){
 		}
 	}
 
-	console.log(randPermutation);
-	return randPermutation;
+	var optionsArragnment = randPermutation;
+	var optionIds = ["answer1", "answer2", "answer3"];
+
+	$('#option1').attr("id", optionIds[optionsArragnment[0]]);
+	$('#option2').attr("id", optionIds[optionsArragnment[1]]);
+	$('#option3').attr("id", optionIds[optionsArragnment[2]]);
 }
 
 var score = 0;
